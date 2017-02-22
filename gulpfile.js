49db98c67baa543f468a3fd3d2281692cb2d642a
Will Arends <willarends@gmail.com>
2017-02-22 11:49:56 -0800

    scrapped webpack, back to gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    sass = require('gulp-sass'),
+    // jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css');
+
+
+gulp.task('js', function () {
+  gulp.src(['public/app/app.js', 'public/app/**/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.min.js'))
+      // .pipe(jshint())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/dist/js'))
+});
+
+gulp.task('js-prod', function () {
+  gulp.src(['public/dist/js/app.min.js'])
+      .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src('public/css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/dist/styles'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['public/app/**/*.js', 'app/**/*.js'], ['js']);
+  gulp.watch('public/css/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('serve', ['watch'], function () {
+  nodemon({
+    script: 'server.js',
+    ext: 'html js scss'
+  })
+  .on('restart', function () {
+      console.log('gulp restarted!');
+    });
+});
--- public/app/home/homeCtrl.js
+++ public/app/home/homeCtrl.js
@@ -1,9 +1,17 @@
-// angular.module('hb-interiors').controller('homeCtrl', ['$scope', function($scope){
-//   console.log('Home Ctrl');
-// }]);
+angular.module('hb-interiors').controller('homeCtrl', ['$scope', function($scope){
+  console.log('working!');
+}]);
+//
+// export default class homeCtrl {
+//   constructor(){
+//     console.log('Home Controller!!!!!');
+//   }
+// }
 
-export default class homeCtrl {
-  constructor(){
-    console.log('Home Controller!!!!!');
-  }
-}
+// export default function($scope) {
+//   console.log('this txfghdfghfgfdghdfg working?');
+//
+//   $scope.buttonClick = () => {
+//     console.log('clicked');
+//   }
+// }
