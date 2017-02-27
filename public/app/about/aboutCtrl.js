77dd515df5b23cddc860c13fde7b87eff6df5251
Will Arends <willarends@gmail.com>
2017-02-27 11:41:34 -0800

    services scafold

--- public/app/about/aboutCtrl.js
+++ public/app/about/aboutCtrl.js
@@ -3,8 +3,7 @@ app.controller('aboutCtrl', ['$scope', ($scope) => {
   $scope.isActive = false;
   $scope.minusSign = false;
   $scope.expandBio = () => {
-      $scope.minusSign = !$scope.minusSign;
-      $scope.isActive = !$scope.isActive;
+      $scope.minusSign = !$scope.minusSign && $scope.isActive = !$scope.isActive;
   };
 
 }]);
