67d7d4f0c7c0bd767c0546e0ecc2710e8606e6bc
Will Arends <willarends@gmail.com>
2017-04-18 22:52:35 -0700

    meta data controller

---
+++ public/app/common/meta.controller.js
@@ -0,0 +1,14 @@
+app.factory('Meta', function(){
+  var title = 'Hayley Bagwell Interior Design, Seattle, WA';
+  var desc = 'Super DERRR';
+  return {
+    title: function() {return title;},
+    setTitle: function(newTitle) { title = newTitle },
+    description: function() { return desc;},
+    setDesc : function(newDesc) { desc = newDesc}
+  }
+})
+.controller('MetaController', ['$scope', 'Meta', function($scope, Meta){
+  $scope.Meta = Meta;
+  $scope.metaDesc = Meta;
+}]);
