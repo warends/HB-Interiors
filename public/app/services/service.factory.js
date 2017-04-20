d94b07a110fbfde38245bae94f728ea689512bdc
Will Arends <willarends@gmail.com>
2017-04-19 17:24:56 -0700

    services detail

--- public/app/services/service.factory.js
+++ public/app/services/service.factory.js
@@ -10,18 +10,23 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
       logo: 'img/icons/house-plan.png',
       banner: 'img/services/floor-plan.png',
       subHead: 'creating individualized products',
-      desc: 'We can create a functional floor plan that has great flow.  Send us images of your space with measurements to allow us the ability to bring your space new comfort and functionality.'
+      desc: 'We can help make your space more functional with our space planning service.  Contact us for more details.'
     },
-    // {
-    //   name: 'Branding'
-    // },
+    {
+      name: 'Branding',
+      slug: 'branding',
+      logo: 'img/icons/branding.png',
+      banner: 'img/services/branding.jpg',
+      subHead: 'Helping your customers get to know you',
+      desc: 'We understand that it is difficult to separate yourself from others.  With our custom made booklets, we will create a beautiful marketing and branding tool to be able to physically hand to potential clients, architects, magazines, or anyone else you want to present your work to. We take the amazing images of your designs and put together a distict booklet that displays your beautiful work.  Take a look at some of the talented designers that we’ve worked with below.  Please contact us for more details.'
+    },
     {
       name: 'Interior Design',
       slug: 'interior-design',
       logo: 'img/icons/think-draw-build.png',
       banner: 'img/services/master-br.png',
       subHead: 'creating individualized products',
-      desc: 'We would love to come to your home and give you a free hour consultation.  We will create a new and inspiring space that emulates your personality.'
+      desc: 'Contact us for full service in-person design services.  It is most important to us to create a design that reflects the personalities of our wonderful clients.  Please contact us for more details.'
     },
     {
       name: 'Furniture Design',
@@ -29,7 +34,7 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
       logo: 'img/icons/construction.png',
       banner: 'img/services/bench.png',
       subHead: 'creating individualized products',
-      desc: 'We love to create furniture out of sustainable materials. Crafting unique furniture has become a wondeful passion of our.  Let us help you create something one-of-a-kind for your space.'
+      desc: 'Whether it’s creating unique furniture from an amazing piece of wood or refinishing a great piece of furniture with good bones, we love it.  If there’s something that is a keepsake from your grandmother that you feel bad parting with, let us take a look and see if we can’t remake it into something that you love.'
     }
   ];
 
