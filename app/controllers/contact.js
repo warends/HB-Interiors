8321d37cff3d08442a3d2d7c26d639df2448718d
Will Arends <willarends@gmail.com>
2017-05-01 22:52:04 -0700

    contact form api key setup

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,39 +1,39 @@
 var nodemailer = require('nodemailer');
 
-// var generator = require('xoauth2').createXOAuth2Generator({
-//   user: 'haybagwell@gmail.com',
-//   clientId: process.env.GMAIL_CLIENT_ID,
-//   clientSecret: process.env.GMAIL_SECRET,
-//   refreshToken: '1/Q8J7ncJl9WqafJT1AhSirojrLF2HLZeY9LEvI_MvsQjOX9C1HiOzr0FhP_Si3GKL',
-//   accessToken: 'ya29.GlsWBFZmSut6xdXbPHG4Conerz62LEQi0vLdcqbNDll2-NKTRgsXGtQ4yZcwdKdhP_wzXubvGDpNEkxxRvYsUlApsmaFXPjXLzjvBzKrMFGi-zDfrngjs8xdxm1_'
-// });
+var generator = require('xoauth2').createXOAuth2Generator({
+  user: 'haybagwell@gmail.com',
+  clientId: process.env.GMAIL_HB_CLIENT_ID,
+  clientSecret: process.env.GMAIL_HB_SECRET,
+  refreshToken: '1/QjpliYhZzIvA3tJ5Dsid2kKVgXu37e1KXeuLa3lUkAk',
+  accessToken: 'ya29.Gls-BO2ZnJFyBtJMJnsnVF8AQygLAw6VYrzy729amWQHozoM3XwHYUh4NZCcdrWfysENARJx6fOqLMGJPcPgPtsFO5s4qYI9q2k-ZtsppVYiQ4XJq_wOEiPNZVOw'
+});
 
 //listen for token updates (if refreshToken is set) you probably want to store these to a db
-// generator.on('token', function(token){
-//     console.log('New token for %s: %s', token.user, token.accessToken);
-// });
-//
-// var transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//           xoauth2: generator
-//       },
-//       debug: true
-// });
-//
-// transporter.verify(function(error, success) {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Server is ready to take our messages');
-//   }
-// });
+generator.on('token', function(token){
+    console.log('New token for %s: %s', token.user, token.accessToken);
+});
+
+var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          xoauth2: generator
+      },
+      debug: true
+});
+
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take our messages');
+  }
+});
 
 // transporter.sendMail({
 //   from: 'sender@example.com',
-//   to: 'willarends@gmail.com',
-//   subject: 'Working',
-//   text: 'This is never gonna work',
+//   to: 'haybagwell@gmail.com',
+//   subject: 'Test',
+//   text: 'This email working???',
 // });
 
 exports.sendMail = function(req, res){
