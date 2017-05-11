11c7bcc3adbb3c11877e17758f343f8dc5be0eba
Will Arends <Will.Arends@indigoslate.com>
2017-05-11 13:45:49 -0700

    view directive properties

--- public/app/common/inview.directive.js
+++ public/app/common/inview.directive.js
@@ -1,16 +1,18 @@
 angular.module('common.inview', []).directive('inView', ['$window', ($window) => {
     return {
         restrict: 'A',
+        scope: {
+            viewObj: '='
+        },
         link: (scope, element, attrs) => {
             angular.element($window).bind('scroll', () => {
                 var $el = element[0],
-                    pos = $el.getBoundingClientRect(),
-                    offset = 0;
-                if(pos.top + offset < $window.scrollY) {
-                    scope.view = true;
-                    console.log('in view :' + scope.view);
+                    pos = $el.getBoundingClientRect();
+                if(pos.top + scope.viewObj.offset < $window.scrollY) {
+                    scope.viewObj.visible = true;
+                    console.log('in view :' + scope.viewObj.visible);
                 } else {
-                    scope.view = false;
+                    scope.viewObj.visible = false;
                 }
                 scope.$apply();
             });
--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -2,4 +2,26 @@ angular.module('home.controller', []).controller('HomeController', ['$scope', 'S
 
   $scope.services = ServiceFactory.list();
 
+  $scope.welcome = {
+      visible: false,
+      offset: -200
+  };
+
+  $scope.options = {
+   origin: 'left',
+   distance: '150px',
+   easing: 'ease-in-out',
+   delay: 30,
+   scale: 1,
+   duration: 1000,
+   // reset: true,
+   // afterReveal: function (domEl) {
+   //   document.getElementById("gallery").style.visibility = "visible";
+   // },
+   sequence: {
+      selector: '.col-sm-5',
+      interval: 300
+    }
+ };
+
 }]);
