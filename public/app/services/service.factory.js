1764abfb0eae6cfc2d99b7796753422b375c8f2a
Will Arends <Will.Arends@indigoslate.com>
2017-05-11 08:43:05 -0700

    services content

--- public/app/services/service.factory.js
+++ public/app/services/service.factory.js
@@ -10,15 +10,15 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
       logo: 'img/icons/house-plan.png',
       banner: 'img/services/floor-plan.png',
       subHead: 'creating individualized products',
-      desc: 'We can help make your space more functional with our space planning service.  Contact us for more details.'
-    },
+      desc: "What is it? Do you feel like your space is not functional? Would you like assistance arranging a layout for your space? We can help!\n \nWe will organize your existing furniture in a way that creates a more natural and functional flow to make the best use of your space."
+  },
     {
       name: 'Branding',
       slug: 'branding',
       logo: 'img/icons/branding.png',
       banner: 'img/services/branding.jpg',
       subHead: 'Helping your customers get to know you',
-      desc: 'We understand that it is difficult to separate yourself from others.  With our custom made booklets, we will create a beautiful marketing and branding tool to be able to physically hand to potential clients, architects, magazines, or anyone else you want to present your work to. We take the amazing images of your designs and put together a distict booklet that displays your beautiful work.  Take a look at some of the talented designers that we’ve worked with below.  Please contact us for more details.'
+      desc: "It is important to be able to stand out from the rest of the designers in your area.  By creating branding tools that potential clients or architects can get their hands on moves you to the front of the line.\n \nWe develop these business tools, such as portfolio booklets and business cards to reach your specific audience."
     },
     {
       name: 'Interior Design',
@@ -26,7 +26,7 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
       logo: 'img/icons/think-draw-build.png',
       banner: 'img/services/master-br.png',
       subHead: 'creating individualized products',
-      desc: 'Contact us for full service in-person design services.  It is most important to us to create a design that reflects the personalities of our wonderful clients.  Please contact us for more details.'
+      desc: "If you are local to the Seattle area, we are here to assist you in your design needs.  We want you to feel at home in your home.  It is very important for us to get to know you and your style.  We will then be able to develop a design that is a reflection of you.\n \nThis is a more traditional form of design.  We will get to know you further by meeting and discussing your design needs and desires.  A design package with concepts will be created all the way through installation."
     },
     {
       name: 'Furniture Design',
@@ -34,7 +34,7 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
       logo: 'img/icons/construction.png',
       banner: 'img/services/bench.png',
       subHead: 'creating individualized products',
-      desc: 'Whether it’s creating unique furniture from an amazing piece of wood or refinishing a great piece of furniture with good bones, we love it.  If there’s something that is a keepsake from your grandmother that you feel bad parting with, let us take a look and see if we can’t remake it into something that you love.'
+      desc: "We love producing industrial, rustic and sustainable pieces.  These can be brand new pieces or repurposed furniture.  Breathing new life into second hand furniture is not only rewarding, but also helps to keep furniture from uselessly going to the trash.\n \nIf there were a certain piece of furniture you are looking for, we would love to be part of the process to create that perfect piece for you."
     }
   ];
 
