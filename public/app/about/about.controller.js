c963a61f00c83ece4ff526c2fb5abbd4d92a02e3
Will Arends <willarends@gmail.com>
2017-06-06 23:20:00 -0700

    mobile home and about page

--- public/app/about/about.controller.js
+++ public/app/about/about.controller.js
@@ -1,10 +1,5 @@
 angular.module('about.controller', []).controller('AboutController', ['$scope', ($scope) => {
 
-  $scope.isActive = false;
-  $scope.minusSign = false;
-  $scope.expandBio = () => {
-      $scope.minusSign = !$scope.minusSign;
-      $scope.isActive = !$scope.isActive;
-  };
+ 
 
 }]);
