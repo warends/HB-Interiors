86f900fd3b3d55f7e9aaa703184133f90b5e587c
Will Arends <willarends@gmail.com>
2017-06-25 09:03:13 -0700

    new icons and text edits

--- public/app/services/service.factory.js
+++ public/app/services/service.factory.js
@@ -7,40 +7,40 @@ angular.module('service.factory', []).factory('ServiceFactory', () => {
     {
       name: 'Space Planning',
       slug: 'space-planning',
-      logo: 'img/icons/house-plan.png',
+      logo: 'img/icons/measurement.svg',
       banner: 'img/services/floor-plan.png',
       subHead: 'creating individualized products',
-      desc: "What is it? Do you feel like your space is not functional? Would you like assistance arranging a layout for your space? We can help!\n \nWe will organize your existing furniture in a way that creates a more natural and functional flow to make the best use of your space.",
+      desc: "What is space planning? Do you feel like your space is not functional? Would you like assistance arranging the layout of your space? We are here to help! We will organize your existing furniture in a way that creates a more natural and functional flow with a customized floor plan that you will love.",
       img1: 'img/services/bubble1.png',
       img2: 'img/services/floorplan.png'
   },
     {
       name: 'Branding',
       slug: 'branding',
-      logo: 'img/icons/branding.png',
+      logo: 'img/icons/finger-print.svg',
       banner: 'img/services/branding.jpg',
       subHead: 'Helping your customers get to know you',
-      desc: "It is important to be able to stand out from the rest of the designers in your area.  By creating branding tools that potential clients or architects can get their hands on moves you to the front of the line.\n \nWe develop these business tools, such as portfolio booklets and business cards to reach your specific audience.",
+      desc: "It is important to be able to stand out.  By creating branding tools that potential clients or architects can get their hands on, we will help move you to the front of the line. We can develop business tools that reflect your vision and reach your specific audience, such as portfolio booklets and business cards.",
       img1: 'img/services/branding1.png',
       img2: 'img/services/branding2.png'
     },
     {
       name: 'Interior Design',
       slug: 'interior-design',
-      logo: 'img/icons/think-draw-build.png',
+      logo: 'img/icons/home.svg',
       banner: 'img/services/master-br.png',
       subHead: 'creating individualized products',
-      desc: "If you are local to the Seattle area, we are here to assist you in your design needs.  We want you to feel at home in your home.  It is very important for us to get to know you and your style.  We will then be able to develop a design that is a reflection of you.\n \nThis is a more traditional form of design.  We will get to know you further by meeting and discussing your design needs and desires.  A design package with concepts will be created all the way through installation.",
+      desc: "Local to the Seattle area? We are here to assist you in your design needs. We want you to feel at home in your home. We’d love to get to know you to develop a style that is all your own. A design package with concepts and a floor plan will be created all the way through installation. Contact us for a free consultation!",
       img1: 'img/services/kitchen.png',
       img2: 'img/services/interior-design.png'
     },
     {
       name: 'Furniture Design',
       slug: 'furniture',
-      logo: 'img/icons/construction.png',
+      logo: 'img/icons/chair1.svg',
       banner: 'img/services/bench.png',
       subHead: 'creating individualized products',
-      desc: "We love producing industrial, rustic and sustainable pieces.  These can be brand new pieces or repurposed furniture.  Breathing new life into second hand furniture is not only rewarding, but also helps to keep furniture from uselessly going to the trash.\n \nIf there were a certain piece of furniture you are looking for, we would love to be part of the process to create that perfect piece for you.",
+      desc: "We love producing industrial, rustic and sustainable pieces here in Seattle. These can be brand new pieces or repurposed furniture. If you have an heirloom piece that you cannot bare to throw away, we may be able to give it a new look.  We would love to be part of the process to create that perfect piece for you.",
       img1: 'img/services/bench-paint.png',
       img2: 'img/services/table-work.png'
     }
