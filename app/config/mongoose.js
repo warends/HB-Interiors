da1bbffd0d7dc59c10eb90aa1631afd3e0392800
warends <willarends@gmail.com>
2017-12-26 22:37:26 -0800

    flip book init

--- app/config/mongoose.js
+++ app/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 module.exports = function(config){
   mongoose.promise = global.Promise;
-  mongoose.connect(config.db);
+  mongoose.createConnection(config.db);
 
   var db = mongoose.connection;
   db.on('error', console.log.bind(console, 'connection error'));
--- public/app/services/service.controller.js
+++ public/app/services/service.controller.js
@@ -1,26 +1,40 @@
 angular.module('service.controller', []).controller('ServiceController', ['$scope', '$stateParams', 'ServiceFactory', 'Meta', ($scope, $stateParams, ServiceFactory, Meta) => {
 
     Meta.setTitle('Services');
-
     $scope.services = ServiceFactory.list();
+
 }]);
 
 angular.module('service.detail.controller', []).controller('SelectedServiceController', ['$scope', '$stateParams', 'ServiceFactory', ($scope, $stateParams, ServiceFactory) => {
-  $scope.selectedService = ServiceFactory.find($stateParams.slug);
-  $scope.branding = false;
-  $scope.branding = ($scope.selectedService.name === 'Branding') ? true : false;
+    
+    $scope.selectedService = ServiceFactory.find($stateParams.slug);
+    $scope.branding = false;
+    $scope.branding = ($scope.selectedService.name === 'Branding') ? true : false;
 
-  $scope.bookShow = false;
-  $scope.rotate = false;
-
-  $scope.toggleBook = function(){
-      console.log('toggle');
+    $scope.bookShow = false;
+    $scope.toggleBook = function(){
       $scope.bookShow = true;
-  }
+    }
 
-  $scope.nextPage = function(){
-      $scope.rotate = !$scope.rotate;
-  }
+    $scope.nextPage = function(){
+      $('.active')
+        .removeClass('active')
+        .addClass('flipped')
+        .next('.page')
+        .addClass('active')
+        .siblings();
+    }
+
+    $scope.prevPage = function(){
+      $('.flipped')
+        .last()
+        .removeClass('flipped')
+        .addClass('active')
+        .siblings('.page')
+        .removeClass('active');
+    }
+
+    $scope.currentPage = 0;
 
 
 }]);
